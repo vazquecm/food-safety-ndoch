5f4ea19fc3b0c53ccee3a01d6c82af0e5b0bfefc
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-06-04 14:06:32 -0500

    fixed stuff

---
+++ app.js
@@ -0,0 +1,19 @@
+// handles the routes for the client side, all client files are in the "public" folder //
+var app = angular.module('mainApp', ['ngRoute'])
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'loginCtrl'
+      })
+      .when('/search-view', {
+        templateUrl: 'partials/search-view.html',
+        controller: 'search-viewCtrl'
+      })
+
+      .otherwise('/login');
+
+  }]);
